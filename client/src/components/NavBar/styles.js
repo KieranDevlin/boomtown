0c68c25014cbb4254b447a0f16d278cd787c152f
Kieran Devlin <kjrdevlin@gmail.com>
2020-01-31 14:46:57 -0800

    remove comment code, add icon, button, and logo styles

--- client/src/components/NavBar/styles.js
+++ client/src/components/NavBar/styles.js
@@ -1,14 +1,21 @@
-// import { createStyles } from '@material-ui/core/styles';
-
-// const styles = createStyles({
 const styles = theme => ({
   nav: {
-    backgroundColor: theme.palette.primary.main,
-    height: 75,
     display: 'flex',
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 20
+    alignItems: 'center'
+  },
+  navButton: {
+    borderRadius: 50
+  },
+  addIcon: {
+    marginRight: 20,
+    borderRadius: 50,
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.secondary.main
+  },
+  logo: {
+    height: 40
   }
 });
 
