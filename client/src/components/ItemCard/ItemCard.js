c03f29178ac34486d3e040d42e829a2ae632b71d
Kieran Devlin <kjrdevlin@gmail.com>
2020-01-31 14:51:35 -0800

    add Gravatar and Momentjs, remove @materialui avatar

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -1,15 +1,16 @@
 import { withStyles } from '@material-ui/core/styles';
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
+import moment from 'moment';
+
 import {
-  Avatar,
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia
 } from '@material-ui/core';
-
+import Gravatar from 'react-gravatar';
 import styles from './styles';
 
 const ItemCard = ({
@@ -19,19 +20,50 @@ const ItemCard = ({
   description,
   created,
   tags,
-  classes
+  classes,
+  itemowner
 }) => {
+  const date = moment(created)
+    .startOf('day')
+    .fromNow();
+
   return (
     <Card className={classes.card}>
-      <CardMedia className={classes.media} style={{ backgroundColor: 'blue' }}>
-        {/* <img src={imageurl}></img> */}
-      </CardMedia>
+      <CardMedia
+        className={classes.media}
+        style={{
+          background: `url(https://picsum.photos/400/300)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      ></CardMedia>
       <CardContent className={classes.innerCard}>
-        <Avatar
+        <div style={{ display: 'flex', alignContent: 'center' }}>
+          <Gravatar
+            email={itemowner.email}
+            size={80}
+            rating="pg"
+            default="monsterid"
+            className="CustomAvatar-image"
+            style={{ borderRadius: 50, marginRight: 20 }}
+          />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center'
+            }}
+          >
+            <Typography>{itemowner.fullname}</Typography>
+            <Typography>{date}</Typography>
+          </div>
+        </div>
+
+        {/* <Avatar
           className={classes.user}
           alt="Remy Sharp"
           src="https://cdn.auth0.com/blog/react-js/react.png"
-        />
+        /> */}
         <CardContent className={classes.content}>
           <Typography variant="h5">{title}</Typography>
           {tags.map(tag => {
