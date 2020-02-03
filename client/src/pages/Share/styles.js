8901eb7e2459a7f6ad14d56653fad65fdba5faa3
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-03 13:11:45 -0800

    add styles to share page

--- client/src/pages/Share/styles.js
+++ client/src/pages/Share/styles.js
@@ -1,3 +1,9 @@
-const styles = theme => ({});
+const styles = theme => ({
+  root: {
+    paddingTop: 120,
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  }
+});
 
 export default styles;
