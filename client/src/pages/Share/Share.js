0f133de3af2e57ec1302412366170d2ebe8ee905
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-03 13:12:33 -0800

    remove comment code, add ShareItemPreview and Form components

--- client/src/pages/Share/Share.js
+++ client/src/pages/Share/Share.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-/* 
-  TODO: Create ShareItemFrom and ShareItemPreview in the components dir
-  and call them from this file.
+import ShareItemForm from '../../components/ShareItemForm';
+import ShareItemPreview from '../../components/ShareItemPreview';
 
-  ShareItemForm is the form that our User will use to add a new item 
-
-  When the user is filling ShareItemForm, we will show a preview of 
-  this item using the ShareItemPreview. 
-  Hint: It should look like any other Item card.
-
-*/
-// import ShareItemForm from '../../components/ShareItemForm';
-// import ShareItemPreview from '../../components/ShareItemPreview';
-
-const Share = ({ classes }) => {
+const Share = ({ classes, tags }) => {
   return (
-    <div>
-      <p>
-        This is the share page located at <code>/share</code>.
-      </p>
+    <div className={classes.root}>
+      <ShareItemPreview />
+      <ShareItemForm tags={tags} />
     </div>
   );
 };
