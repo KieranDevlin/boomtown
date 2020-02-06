a8d4420f30fd3666e68678ea40a6013f9b2659bb
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-06 08:28:35 -0800

    Wrap in Consumer to access logged in user ID, add FullScreenLoader

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import Items from './Items';
-// import FullScreenLoader from '../../components/FullScreenLoader';
+import FullScreenLoader from '../../components/FullScreenLoader';
 import { Query } from 'react-apollo';
 import { ALL_ITEMS_QUERY } from '../../apollo/queries';
+import { ViewerContext } from '../../context/ViewerProvider';
 
 class ItemsContainer extends Component {
   render() {
     return (
-      <Query query={ALL_ITEMS_QUERY} variables={{ filter: 1 }}>
-        {({ loading, error, data }) => {
-          const { items } = data;
-          // if (loading) return <FullScreenLoader />;
-          if (loading) return 'Loading...';
-          if (error) return `Error! ${error.message}`;
-          return <Items items={items} />;
+      <ViewerContext.Consumer>
+        {({ viewer, loading }) => {
+          if (loading) return <FullScreenLoader />;
+          return (
+            <Query query={ALL_ITEMS_QUERY} variables={{ filter: viewer.id }}>
+              {({ loading, error, data }) => {
+                const { items } = data;
+                if (loading) return <FullScreenLoader />;
+                if (error) return `Error! ${error.message}`;
+                return <Items items={items} />;
+              }}
+            </Query>
+          );
         }}
-      </Query>
+      </ViewerContext.Consumer>
     );
   }
 }
