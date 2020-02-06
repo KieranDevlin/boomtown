2166b754723f518ebd79295281fd1d68bd9c54c1
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-06 08:38:41 -0800

    Add ItemPreview and Viewer context providers

--- client/src/index.js
+++ client/src/index.js
@@ -7,33 +7,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import theme from './theme';
-/**
- * @TODO: Add Routing
- *
- * Uncomment the following line when your routes are configured
- *
- *
- * Below in your <App />, nest your <AppRoutes /> inside of <BrowserRouter />
- * component to enable routing in your client app.
- */
-
-/**
- * @TODO: Wrap your app with the Item Preview Provider
- *
- * import ItemPreviewProvider from './context/ItemPreviewProvider'
- *
- * Wrap this component around your app to access Item Preview Context API.
- */
-
-/**
- * @TODO: Wrap your app with the Viewer Context
- *
- * import ViewerProvider from './context/ViewerProvider'
- *
- * Below in your <App />, wrap the <ViewerProvider /> component around
- * the <BrowserRouter /> component so the router is aware of whether a
- * user is currently logged in and who that user is.
- */
+import ItemPreviewProvider from './context/ItemPreviewProvider';
+import ViewerProvider from './context/ViewerProvider';
 
 import './index.css';
 
@@ -43,9 +18,13 @@ const App = () => {
       <CssBaseline />
 
       <ApolloProvider client={client}>
-        <Router>
-          <AppRoutes />
-        </Router>
+        <ViewerProvider>
+          <ItemPreviewProvider>
+            <Router>
+              <AppRoutes />
+            </Router>
+          </ItemPreviewProvider>
+        </ViewerProvider>
       </ApolloProvider>
     </MuiThemeProvider>
   );
