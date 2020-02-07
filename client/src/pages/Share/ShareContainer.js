bb73ee409ae5d75233b95590e7462d9a35d79e0f
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-07 15:59:36 -0800

    Add context to access viewer props in SharePreview

--- client/src/pages/Share/ShareContainer.js
+++ client/src/pages/Share/ShareContainer.js
@@ -3,20 +3,26 @@ import Share from './Share';
 import FullScreenLoader from '../../components/FullScreenLoader';
 import { Query } from 'react-apollo';
 import { ALL_TAGS_QUERY } from '../../apollo/queries';
-// Hint: query tags
+import { ViewerContext } from '../../context/ViewerProvider';
 
 class ShareContainer extends Component {
   render() {
     return (
-      <Query query={ALL_TAGS_QUERY}>
-        {({ loading, error, data }) => {
+      <ViewerContext.Consumer>
+        {({ viewer, loading }) => {
           if (loading) return <FullScreenLoader />;
-          if (error) return `Error! ${error.message}`;
-          if (data) return <Share tags={data.tags} />;
+          return (
+            <Query query={ALL_TAGS_QUERY}>
+              {({ loading, error, data }) => {
+                if (loading) return <FullScreenLoader />;
+                if (error) return `Error! ${error.message}`;
+                if (data) return <Share tags={data.tags} viewer={viewer} />;
+              }}
+            </Query>
+          );
         }}
-      </Query>
+      </ViewerContext.Consumer>
     );
   }
 }
-
 export default ShareContainer;
