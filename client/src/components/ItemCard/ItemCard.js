631c17e2d71b524d4fdb050b7163bc33f125dbf9
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-07 16:18:27 -0800

    Add viewer logic

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -1,84 +1,104 @@
 import { withStyles } from '@material-ui/core/styles';
 import React from 'react';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 import {
   Button,
   Box,
   Card,
   CardActions,
-  CardActionArea,
   CardContent,
   CardMedia,
-  Link,
-  Typography
+  Typography,
+  Zoom
 } from '@material-ui/core';
 import Gravatar from 'react-gravatar';
 import styles from './styles';
 
-const ItemCard = ({ item, classes }) => {
+const ItemCard = ({ item, classes, viewer }) => {
   return (
-    <Card className={classes.card} raised>
-      <Link
-        href={`/profile/${item && item.itemowner.id}`}
-        color="secondary"
-        underline="none"
-      >
-        <CardMedia
-          component="img"
-          src={
-            item && item.imageurl
-              ? item.imageurl
-              : 'http://via.placeholder.com/350x250?text=No+image'
-          }
-          className={classes.media}
-        />
-      </Link>
-      <CardContent className={classes.innerCard}>
-        <Box className={classes.itemInfoContainer}>
-          <Gravatar
-            email={item && item.itemowner && item.itemowner.email}
-            size={70}
-            rating="pg"
-            default="retro"
-            className={`CustomAvatar-image ${classes.customGravatar}`}
+    <Zoom in={true}>
+      <Card className={classes.card} raised>
+        <Link
+          to={`/profile/${
+            viewer ? viewer.id : item && item.itemowner && item.itemowner.id
+          }`}
+          color="secondary"
+          underline="none"
+        >
+          <CardMedia
+            component="img"
+            src={
+              item && item.imageurl
+                ? item.imageurl
+                : 'http://via.placeholder.com/350x250?text=No+image'
+            }
+            className={classes.media}
           />
-          <Box className={classes.itemInfo}>
-            <Link
-              href={`/profile/${item && item.itemowner.id}`}
-              color="secondary"
-            >
-              {item && item.itemowner && item.itemowner.fullname}
-            </Link>
-            <Typography>
-              {item &&
-                moment(item && item.created)
-                  .startOf('day')
-                  .fromNow()}
-            </Typography>
+        </Link>
+        <CardContent className={classes.innerCard}>
+          <Box className={classes.itemInfoContainer}>
+            <Gravatar
+              email={
+                viewer
+                  ? viewer.email
+                  : item && item.itemowner && item.itemowner.email
+              }
+              size={70}
+              rating="pg"
+              default="retro"
+              className={`CustomAvatar-image ${classes.customGravatar}`}
+            />
+            <Box className={classes.itemInfo}>
+              <Link
+                to={`/profile/${
+                  viewer
+                    ? viewer.id
+                    : item && item.itemowner && item.itemowner.id
+                }`}
+                color="secondary"
+              >
+                {viewer
+                  ? viewer.fullname
+                  : item && item.itemowner && item.itemowner.fullname}
+              </Link>
+              <Typography>
+                {item &&
+                  moment(item && item.created)
+                    .startOf('day')
+                    .fromNow()}
+              </Typography>
+            </Box>
           </Box>
-        </Box>
 
-        <CardContent className={classes.content}>
-          <Typography variant="h5">{item && item.title}</Typography>
-          {item &&
-            item.tags &&
-            item.tags.map(tag => {
-              return (
-                <Typography variant="caption" key={tag.id}>
-                  {tag.title}
-                </Typography>
-              );
-            })}
-          <Typography>{item && item.description}</Typography>
+          <CardContent className={classes.content}>
+            <Typography variant="h5">{item && item.title}</Typography>
+            <Box>
+              {item &&
+                item.tags &&
+                item.tags.map(tag => {
+                  return (
+                    <Typography
+                      variant="caption"
+                      key={tag.id}
+                      className={classes.tags}
+                    >
+                      {tag.title}
+                    </Typography>
+                  );
+                })}
+            </Box>
+            <Typography>{item && item.description}</Typography>
+          </CardContent>
+
+          <CardActions className={classes.buttonContainer}>
+            <Button variant="outlined" size="medium" color="secondary">
+              Borrow
+            </Button>
+          </CardActions>
         </CardContent>
-
-        <CardActions className={classes.buttonContainer} disableSpacing={true}>
-          <Button variant="outlined" size="medium" color="secondary">
-            Borrow
-          </Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+      </Card>
+    </Zoom>
   );
 };
 
