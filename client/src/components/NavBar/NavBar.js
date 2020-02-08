e7f82c38bc0c6dd2c1ff7022137d7bebde607b69
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-07 16:16:04 -0800

    Fix href bug using Links

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from '../../images/boomtown.svg';
-import { withRouter } from 'react-router-dom';
+import { withRouter, NavLink, Link } from 'react-router-dom';
 import {
   AppBar,
   Button,
+  Box,
   IconButton,
-  Link,
   Menu,
   MenuItem,
   Toolbar,
@@ -19,11 +19,8 @@ import {
   PowerSettingsNew
 } from '@material-ui/icons/';
 import styles from './styles';
-
-const options = [
-  { id: 0, icon: Fingerprint, label: 'Profile', path: '/profile' },
-  { id: 1, icon: PowerSettingsNew, label: 'Sign Out', path: '/home' }
-];
+import { Mutation } from 'react-apollo';
+import { LOGOUT_MUTATION, VIEWER_QUERY } from '../../apollo/queries';
 
 const ITEM_HEIGHT = 48;
 
@@ -36,78 +33,85 @@ const NavBar = ({ classes, location }) => {
   };
 
   const handleClose = value => {
-    console.log(value);
     setAnchorEl(null);
   };
 
-  return (
-    <Slide in={location.pathname !== '/home'}>
-      <AppBar>
-        <Toolbar className={classes.nav}>
-          <Link href="/items">
-            <img
-              src={logo}
-              className={classes.logo}
-              alt="BoomTown logo - return to home."
-            />
-          </Link>
-          <div>
-            <Slide in={location.pathname !== '/share'}>
-              <Button
-                size="large"
-                color="secondary"
-                href="/share"
-                className={classes.navButton}
-              >
-                <Add className={classes.addIcon} />
-                Share Something
-              </Button>
-            </Slide>
+  const refetchQueries = [{ query: VIEWER_QUERY }];
 
-            <IconButton
-              aria-label="more"
-              aria-controls="long-menu"
-              aria-haspopup="true"
-              onClick={handleClick}
-            >
-              <MoreVert />
-            </IconButton>
-            <Menu
-              id="long-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={open}
-              onClose={handleClose}
-              PaperProps={{
-                style: {
-                  maxHeight: ITEM_HEIGHT * 4.5,
-                  width: 200
-                }
-              }}
-            >
-              {options.map(option => {
-                let IconComponent = option.icon;
-                return (
-                  <Link key={option.id} href={option.path} color="secondary">
-                    <MenuItem
-                      selected={option === 'Profile'}
-                      onClick={handleClose}
-                      style={{
-                        display: 'flex',
-                        justifyContent: 'space-between'
-                      }}
-                    >
-                      <IconComponent />
-                      {option.label}
+  return (
+    <Mutation mutation={LOGOUT_MUTATION} refetchQueries={refetchQueries}>
+      {logout => {
+        return (
+          <Slide in={location.pathname !== '/home'}>
+            <AppBar>
+              <Toolbar className={classes.nav}>
+                <NavLink to="/items">
+                  <Button className={classes.navButton}>
+                    <img
+                      src={logo}
+                      className={classes.logo}
+                      alt="BoomTown logo - return to Items."
+                    />
+                  </Button>
+                </NavLink>
+                <Box>
+                  <NavLink to="/share">
+                    <Slide in={location.pathname !== '/share'}>
+                      <Button
+                        size="large"
+                        color="secondary"
+                        className={classes.navButton}
+                      >
+                        <Add className={classes.addIcon} />
+                        Share Something
+                      </Button>
+                    </Slide>
+                  </NavLink>
+
+                  <IconButton
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                  >
+                    <MoreVert />
+                  </IconButton>
+                  <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                      style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: 125
+                      }
+                    }}
+                  >
+                    <Link to="/profile">
+                      <MenuItem
+                        onClick={handleClose}
+                        color="primary"
+                        className={classes.menuItems}
+                      >
+                        <Fingerprint />
+                        Profile
+                      </MenuItem>
+                    </Link>
+
+                    <MenuItem onClick={logout} className={classes.menuItems}>
+                      <PowerSettingsNew />
+                      Sign Out
                     </MenuItem>
-                  </Link>
-                );
-              })}
-            </Menu>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </Slide>
+                  </Menu>
+                </Box>
+              </Toolbar>
+            </AppBar>
+          </Slide>
+        );
+      }}
+    </Mutation>
   );
 };
 
