5326c2dc5a31c2f7d142a2522da8d77256fd5f49
Kieran Devlin <kjrdevlin@gmail.com>
2020-02-09 11:25:27 -0800

    Update Grid props

--- client/src/pages/Share/Share.js
+++ client/src/pages/Share/Share.js
@@ -9,10 +9,10 @@ const Share = ({ classes, tags, viewer }) => {
   return (
     <Container className={classes.root}>
       <Grid container spacing={6} justify="space-evenly">
-        <Grid item xs={8} md={5} lg={5}>
+        <Grid item xs={5} md={5} lg={5}>
           <ShareItemPreview viewer={viewer} />
         </Grid>
-        <Grid item xs={8} md={5} lg={5}>
+        <Grid item xs={5} md={5} lg={5}>
           <ShareItemForm tags={tags} />
         </Grid>
       </Grid>
